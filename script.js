abeacd0f116fd653cd0738f5b4f2043f26ff7d9e
leticea <nascimento.leticeah@gmail.com>
2022-10-28 16:43:47 -0300

    inserindo funções no js

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculate() {
+
+    let height = document.getElementById("height");
+    let weight = document.getElementById("weight");
+
+    console.log(height);
+    console.log(weight);
+
+}
