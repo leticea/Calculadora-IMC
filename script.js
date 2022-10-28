9f46c0ee69ebb9de75501fd6c5860da83bbf39f7
leticea <nascimento.leticeah@gmail.com>
2022-10-28 17:56:24 -0300

    inserindo funções no js

--- script.js
+++ script.js
@@ -5,29 +5,29 @@ function calculate() {
 
     const imc = weight / height ** 2;
 
-    let result ="";
+    let result = "";
 
     if (imc < 18.5) {
 
-        result = (`${imc}. Está abaixo do peso.`);
+        result = (`IMC:${imc}. Está abaixo do peso.`);
     } 
 
     if (imc > 18.5 && imc < 24.9) {
 
-        result = (`${imc}. Está com o peso normal.`);
+        result = (`IMC:${imc}. Está com o peso normal.`);
     } 
 
     if (imc > 25 && imc < 29) {
 
-        result = (`${imc}. Está com sobrepeso.`);
+        result = (`IMC:${imc}. Está com sobrepeso.`);
     }
 
     if (imc > 30 && imc < 40) {
-        result = (`${imc}. Está com obesidade.`);
+        result = (`IMC:${imc}. Está com obesidade.`);
     }
 
     if (imc > 40) {
-        result = (`${imc}. Está com obesidade grave.`);
+        result = (`IMC:${imc}. Está com obesidade grave.`);
     }
 
     document.getElementById("text-area").innerText=result;
