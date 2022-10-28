25c00773ac64902a389dbe13e8481c4583ad65c2
leticea <nascimento.leticeah@gmail.com>
2022-10-28 18:06:48 -0300

    inserindo funções no js

--- script.js
+++ script.js
@@ -9,25 +9,27 @@ function calculate() {
 
     if (imc < 18.5) {
 
-        result = (`IMC:${imc}. Está abaixo do peso.`);
+        result = (`IMC: ${imc}. Está abaixo do peso.`);
     } 
 
     if (imc > 18.5 && imc < 24.9) {
 
-        result = (`IMC:${imc}. Está com o peso normal.`);
+        result = (`IMC: ${imc}. Está com o peso normal.`);
     } 
 
     if (imc > 25 && imc < 29) {
 
-        result = (`IMC:${imc}. Está com sobrepeso.`);
+        result = (`IMC: ${imc}. Está com sobrepeso.`);
     }
 
     if (imc > 30 && imc < 40) {
-        result = (`IMC:${imc}. Está com obesidade.`);
+
+        result = (`IMC: ${imc}. Está com obesidade.`);
     }
 
     if (imc > 40) {
-        result = (`IMC:${imc}. Está com obesidade grave.`);
+        
+        result = (`IMC: ${imc}. Está com obesidade grave.`);
     }
 
     document.getElementById("text-area").innerText=result;
